5618c791e1d019dab69791388a4026b45c590cd8
Ekaterina Zakharenko <45923491+katyazaharenko@users.noreply.github.com>
2018-12-22 18:48:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <iostream>
+#include <exception>
+#include <windows.h>
+#include "space.h"
+#include "params.h"
+
+using namespace std;
+using namespace spaces;
+
+int main(int argc,char ** argv) {
+	setlocale(LC_ALL, "rus");
+
+	params pars(argc, argv);
+
+	if (pars.isRightInput())
+	{
+		cout << "Поиск наикратчайшего пути в двумерном дискретном пространстве" << endl;
+		Space S1(pars.getStringArg());
+		S1.getSpace();
+
+		if (S1.setStart(pars.getIntegerArg(2), pars.getIntegerArg(3)) && S1.setFinish(pars.getIntegerArg(4), pars.getIntegerArg(5))) {
+			LeeAlgorithm L1;
+			cout << "Попытка найти кратчайший путь..." << endl;
+
+			if (L1.lee(S1)) {
+				cout << "Поиск завершен успешно!" << endl;
+			}
+			else {
+				cout << "Невозможно найти кратчайший путь!" << endl;
+			}
+		}
+		else cout << "Выход точки за границу массива, поиск невозможен." << endl;
+
+	}
+
+	system("pause");
+
+}
+
